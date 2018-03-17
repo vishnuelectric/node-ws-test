f2e7a34a4aa1bbd5032a1c5ba4e76fbc4a355afe
vishnu prasad <vishnu.prasad@databackapp.com>
2018-03-17 12:58:02 +0530

    This is the first commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ console.log("websocket server created")
 
 wss.on("connection", function(ws) {
   var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
+    ws.emit("hello","world");
   }, 1000)
 
   console.log("websocket connection open")
